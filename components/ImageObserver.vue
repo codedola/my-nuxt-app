575ffd2ca03debc6816518e00ed90bafd82a4b79
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-06-17 10:32:11 +0700

    start image lazy load

---
+++ components/ImageObserver.vue
@@ -0,0 +1,23 @@
+<template>
+   <img :data-src="src" :data-src-mb="srcMb || src" :alt="alt" class="lazy-load" />
+</template>
+<script setup>
+import { onMounted } from 'vue'
+defineProps({
+  src: {
+    type: String
+  },
+  srcMb: {
+    type: String
+  },
+  alt: {
+    type: String
+  }
+})
+onMounted(loadImage)
+onMounted(loadImage)
+onMounted(loadImage)
+function loadImage() {
+  console.log('load Image run')
+}
+</script>

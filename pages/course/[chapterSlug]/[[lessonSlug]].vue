7c8bb48410f91c1f275245ea0b3b84f85c167292
Do Vu Thanh Nhan <nhanthanhdovu@gmail.com>
2023-09-02 14:55:36 +0700

    start video 8

--- pages/course/[chapterSlug]/[[lessonSlug]].vue
+++
@@ -1,8 +0,0 @@
-<template lang="">
-  <div>Chapter slug {{ $route.params.chapterSlug }}</div>
-  <div>Lesson slug {{ $route.params.lessonSlug }}</div>
-</template>
-<script>
-export default {};
-</script>
-<style lang=""></style>
